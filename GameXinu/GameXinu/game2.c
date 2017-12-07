7e1d5d262910ba980b1199f1db1368257cb1bb3b
Izhar Ananiev <izhar_ananiev@hotmail.com>
2017-12-07 20:19:58 +0200

    Game over + successfull jump

--- GameXinu/GameXinu/game2.c
+++ GameXinu/GameXinu/game2.c
@@ -7,29 +7,35 @@
 
 /*
 #define BALL_COLOR_NON_BLINK 112
-#define LIGHT_BLUE_BACK 48
 #define PINK_BACK 80
-#define BLACK_BACK 0
 */
 
-#define BALL_SIZE 4			//amount of ball pixels
-#define BALL_COLOR 240		//white blinking color
+#define BALL_SIZE 4					//amount of ball pixels
+#define BALL_COLOR 240				//white blinking color
 #define BALL_VALUE '*'
 
-#define CUBE_SIZE 4			//amount of cube pixels
-#define CUBE_COLOR 32		//green color
+#define CUBE_VALUE 'X'
+#define CUBE_SIZE 4					//amount of cube pixels
+#define CUBE_COLOR 32				//green color
 
 #define TRACK_VALUE ' '
-#define TRACK_COLOR 16		//drak blue color
+#define TRACK_COLOR 16				//drak blue color
+#define TRACK_COLOR_2 48			//light blue color
 
 #define BORDER_VALUE '|'
-#define BORDER_COLOR 131	//light blue foreground blinking color
+#define BORDER_COLOR 131			//light blue foreground blinking color
 
-#define LIGHT_BLUE_BACK 48
+#define INIT_COLOR 0				//black color
+#define WHITE_BLINKING_FOREGROUD 135
+
+#define DEAD_BALL_COLOR 96+128		//blinking orange color
+#define DEAD_BALL_VALUE 'O'
 
 #define SEC 18
 #define MAX_X(y) (65-y)
 #define MIN_X(y) (60-y)
+#define MIN_Y 0
+#define MAX_Y 24
 
 typedef struct position
 {
@@ -43,7 +49,7 @@ int receiver_pid;
 char display_draft_attr[25][80], display_draft_val[25][80];
 POSITION *cube_pos = NULL, *ball_pos = NULL, *tun_pos = NULL;
 
-char display_attr[2001], display_val[2001];
+char display_attr[2000], display_val[2000], jump_attr_save[4], jump_val_save[4];
 
 int uppid, dispid, recvpid;
 
@@ -59,46 +65,53 @@ int sched_arr_pid[5] = { -1 };
 int sched_arr_int[5] = { -1 };
 int pixelColorFlag = 0;					//for painting -> will change the rows the next paint
 
-int freq = SEC / 4;						//time of loading
+int freq = 0;							//time of loading
 int countLoading = 0;
 int cubeExist = 0;
 int cubeEnd = 0;
+int jumpingFlag = 0;
 /*---------------------------------------------------*/
 
 /*---------------------Functions---------------------*/
-
 extern SYSCALL sleept(int);
 extern struct intmap far *sys_imp;
-void change_value_by_pos(int, unsigned char);
-void init_display_adapter_and_board();
-int get_pos(int, int);
-int compare_to_ball_pos(int, int);
-int compare_to_cube_pos(int, int);
-void create_cube();
-void update_cube();
-void display_board();
-void update_board();
-void init_board();
-void finish();
-
-/*(+Or- = {-,+}).
-Get as parameter what we want to de/increase x.
-Returns 1: in track, 0: not in track, -1:error in input*/
-int check_ball_exceeds_x(char);
-
-/*(dir = {a,d})
-Get as parameter the direction to move the ball
-*/
-void update_ball(char);
-
 void displayer();
 INTPROC new_int9(int mdevno);
 void set_new_int9_newisr();
 void receiver();
 void updateter();
 SYSCALL schedule(int no_of_pids, int cycle_length, int pid1, ...);
-/*-----------------------------------------------------------------------------------------------------------------------------------------------*/
 
+/*-----------------------------MY FUNCTIONS-----------------------------------------*/
+
+void change_value_by_pos(int, unsigned char);
+void init_display_adapter_and_board();
+void init_params();
+int get_pos(int, int);
+
+void create_cube();
+void update_cube();
+int compare_to_cube_pos(int, int);
+
+void init_board();
+void init_board_content();
+void display_board();
+void update_board();
+
+void finish();
+void game_over();
+
+/*(+Or- = {-,+}).
+Get as parameter what we want to de/increase x.
+Returns 1: in track, 0: not in track, -1:error in input*/
+int check_ball_exceeds_x(char);
+int compare_to_ball_pos(int, int);
+void create_ball();
+/*(dir = {a,d})
+Get as parameter the direction to move the ball
+*/
+void update_ball(char);
+void ball_jumping();
 /*---------------------------------------------------------------xmain---------------------------------------------------------------------------*/
 
 xmain()
@@ -117,19 +130,23 @@ xmain()
 void updateter()
 {
 
-	int i, x, y, res;
+	int i, x, y, jumpingCnt = -1;
 	char ch;
 
 	while (1)
 	{
-		while(front != -1)
+		if(front != -1)
 		{
 			ch = ch_arr[front];
 			if (front != rear)
 				front++;
 			else
 				front = rear = -1;
-			if ((ch == 'a') || (ch == 'A'))			//left
+			if (ch == 'j' || ch == 'J') {
+				ball_jumping();
+				jumpingCnt = 0;
+			}
+			else if ((ch == 'a') || (ch == 'A'))			//left
 			{
 				if (check_ball_exceeds_x('-') == 1)	//not exceeding from track
 					update_ball(ch);
@@ -141,10 +158,11 @@ void updateter()
 			}
 			else if ((ch == 'w') || (ch == 'W'))	//up
 			{
-				if (freq / 2 > 0) {
+				if (freq / 2 > 0)
 					freq /= 2;
-					schedule(2, freq, dispid, 0, uppid, 29);
-				}
+				else if (freq == 0)	//at start
+					freq = SEC;
+				schedule(2, freq, dispid, 0, uppid, 29);
 			}
 			else if ((ch == 's') || (ch == 'S'))	//down
 			{
@@ -154,7 +172,11 @@ void updateter()
 				}
 			}
 		}
-
+		update_board();
+		if (jumpingFlag == 1 && jumpingCnt >= 5) {
+			ball_jumping();
+			jumpingCnt = -1;
+		}
 		if (cubeEnd == 1)
 		{
 			cubeExist = 0;
@@ -167,15 +189,14 @@ void updateter()
 			countLoading = 0;
 		}
 
-		for (y = 0; y <= 24; y++) {
-			for (x = MIN_X(y); x <= MAX_X(y); x++) {
+		for (y = MIN_Y; y <= MAX_Y; y++){
+			for (x = MIN_X(MAX_Y); x <= MAX_X(MIN_Y); x++){
 				display_attr[get_pos(x, y) / 2] = display_draft_attr[y][x];
 				display_val[get_pos(x, y) / 2] = display_draft_val[y][x];
 			}
 		}
-		update_board();
-		display_attr[2000] = '\0';
-		display_val[2000] = '\0';
+		if (jumpingCnt != -1)
+			jumpingCnt++;
 	} // while(1)
 
 } // updater 
@@ -184,13 +205,11 @@ void updateter()
 void display_board()
 {
 	int x, y, pos;
-	for (y = 0; y <= 24; y++) {
-		for (x = MIN_X(y); x <= MAX_X(y); x++) {
+	for (y = MIN_Y; y <= MAX_Y; y++){
+		for (x = MIN_X(MAX_Y); x <= MAX_X(MIN_Y); x++) {
 			pos = get_pos(x, y);
 			change_value_by_pos(pos + 1, display_attr[pos / 2]);
 			change_value_by_pos(pos, display_val[pos / 2]);
-			/*if (display_attr[pos / 2] == CUBE_COLOR)
-			printf("(%d,%d): [v=%d,a=%d]\n", x, y, display_val[pos / 2], display_attr[pos / 2]);*/
 		}
 	}
 }
@@ -213,18 +232,25 @@ void update_cube()
 	/*check if the cube reached to the end*/
 	for (i = 0; i < CUBE_SIZE; i++)
 	{
-		if (cubeEnd == 1 || (cube_pos[i].y + 1 >= ball_pos[0].y))	//crash of ball
+		if (cubeEnd == 1 || (cube_pos[i].y >= MAX_Y))
 		{
 			display_draft_attr[cube_pos[i].y][cube_pos[i].x] = TRACK_COLOR;
+			display_draft_val[cube_pos[i].y][cube_pos[i].x] = TRACK_VALUE;
 			cubeEnd = 1;
 		}
 	}
 	if (cubeEnd == 0) {
 		for (i = CUBE_SIZE - 1; i >= 0; i--)
 		{
+			if (compare_to_ball_pos(cube_pos[i].x, cube_pos[i].y) == 0) {
+				game_over();
+				return;
+			}
 			display_draft_attr[cube_pos[i].y][cube_pos[i].x] = TRACK_COLOR;
+			display_draft_val[cube_pos[i].y][cube_pos[i].x] = TRACK_VALUE;
 			cube_pos[i].x--;	cube_pos[i].y++;
 			display_draft_attr[cube_pos[i].y][cube_pos[i].x] = CUBE_COLOR;
+			display_draft_val[cube_pos[i].y][cube_pos[i].x] = CUBE_VALUE;
 		}
 	}
 	else
@@ -255,34 +281,61 @@ char dir;
 	}
 }
 
+void ball_jumping()
+{
+	int i;
+	if(jumpingFlag==0)
+	{
+		for (i = 0; i < BALL_SIZE; i++) {
+			display_draft_attr[ball_pos[i].y][ball_pos[i].x] = jump_attr_save[i];
+			display_draft_val[ball_pos[i].y][ball_pos[i].x] = jump_val_save[i];
+			ball_pos[i].y -= 2;
+			jump_attr_save[i] = display_draft_attr[ball_pos[i].y][ball_pos[i].x];
+			jump_val_save[i] = display_draft_val[ball_pos[i].y][ball_pos[i].x];
+			display_draft_attr[ball_pos[i].y][ball_pos[i].x] = BALL_COLOR;
+			display_draft_val[ball_pos[i].y][ball_pos[i].x] = BALL_VALUE;
+		}
+		jumpingFlag = 1;
+	}
+	else
+	{
+		for (i = 0; i < BALL_SIZE; i++) {
+			display_draft_attr[ball_pos[i].y][ball_pos[i].x] = jump_attr_save[i];
+			display_draft_val[ball_pos[i].y][ball_pos[i].x] = jump_val_save[i];
+			ball_pos[i].y += 2;
+			jump_attr_save[i] = display_draft_attr[ball_pos[i].y][ball_pos[i].x];
+			jump_val_save[i] = display_draft_val[ball_pos[i].y][ball_pos[i].x];
+			display_draft_attr[ball_pos[i].y][ball_pos[i].x] = BALL_COLOR;
+			display_draft_val[ball_pos[i].y][ball_pos[i].x] = BALL_VALUE;
+		}
+		jumpingFlag = 0;
+	}
+}
+
 void update_board()
 {
-	int x,y,pos, color;
+	int x, y, color, rowFlag = 0;
 	for (y = 24; y >= 1; y--) {
 		for (x = MIN_X(y); x <= MAX_X(y); x++){
-			pos = get_pos(x, y) + 1;
 			if (compare_to_ball_pos(x, y) == 0)
 				color = BALL_COLOR;
 			else if (compare_to_cube_pos(x, y) == 0)
 				color = CUBE_COLOR;
-			else if (pixelColorFlag == 0)
+			else if (pixelColorFlag <= 1)					//color swap every 2 rows
 			{
-				if (y % 2 == 0)
-					color = TRACK_COLOR;
-				else
-					color = LIGHT_BLUE_BACK;
+				if (rowFlag <= 1)	color = TRACK_COLOR;
+				else				color = TRACK_COLOR_2;
 			}
 			else
 			{
-				if (y%2 == 0)
-					color = LIGHT_BLUE_BACK;
-				else
-					color = TRACK_COLOR;
+				if (rowFlag <= 1)	color = TRACK_COLOR_2;
+				else				color = TRACK_COLOR;
 			}
 			display_draft_attr[y][x] = color;
 		}
+		rowFlag = (rowFlag + 1) % 4;
 	}
-	pixelColorFlag = (pixelColorFlag + 1) % 2;
+	pixelColorFlag = (pixelColorFlag + 1) % 4;				//color swap every 2 updates
 }
 
 void displayer(void)
@@ -365,27 +418,24 @@ INTPROC new_int9(int mdevno)
 		MOV BYTE PTR scan,AH
 		MOV BYTE PTR ascii,AL
 	} //asm
-		if (scan == 75) //Left
+	if (scan == 57)			//Space
+		result = 'j';//jump
+	else if (scan == 75)		//Left
 			result = 'a';
-		else
-			if (scan == 77)//Right
-				result = 'd';
-			else
-				if (scan == 72)		//Up
-					result = 'w';
-				else
-					if (scan == 80)//Down
-						result = 's';
-	if ((scan == 46) && (ascii == 3)) {// Ctrl-C?
-		if (cube_pos != NULL)
-			freemem(cube_pos);
-		if (ball_pos != NULL)
-			freemem(ball_pos);
-		asm{
-			MOV  AX,2
-			INT  10h
-			INT	 27
-		}
+	else if (scan == 77)		//Right
+			result = 'd';
+	else if (scan == 72)		//Up
+			result = 'w';
+	else if (scan == 80)		//Down
+			result = 's';
+	else if ((scan == 46) && (ascii == 3) || scan == 1) {// Ctrl-C?
+			if (cube_pos != NULL)	freemem(cube_pos);	else;
+			if (ball_pos != NULL)	freemem(ball_pos);	else;
+			asm{
+				MOV  AX,2
+				INT  10h
+				INT	 27
+			}
 	}
 	send(receiver_pid, result);
 
@@ -484,42 +534,10 @@ void init_display_adapter_and_board()
 
 void init_board()
 {
-	int i, pos, x, x1, x2, y;
-	for (y = 0; y <= 24; y++) {
-		for (x = MIN_X(y); x <= MAX_X(y); x++) {
-			pos = get_pos(x, y);
-			display_draft_attr[y][x] = display_attr[pos / 2] = TRACK_COLOR;  // blank
-			display_draft_val[y][x] = display_val[pos / 2] = TRACK_VALUE;
-		}
-	}
-
-	ball_pos = (POSITION*)(getmem(BALL_SIZE * sizeof(POSITION)));
-	if (ball_pos == NULL)
-		finish();
-	ball_pos[0].x = 38;	ball_pos[0].y = 23;
-	ball_pos[1].x = 38;	ball_pos[1].y = 24;
-	ball_pos[2].x = 39;	ball_pos[2].y = 23;
-	ball_pos[3].x = 39;	ball_pos[3].y = 24;
-
-	for (i = 0; i<BALL_SIZE; i++)
-	{
-		pos = get_pos(ball_pos[i].x, ball_pos[i].y);
-		display_draft_attr[ball_pos[i].y][ball_pos[i].x] = display_attr[pos / 2] = BALL_COLOR;
-		display_draft_val[ball_pos[i].y][ball_pos[i].x] = display_val[pos / 2] = BALL_VALUE;
-	}
-
-	/*Blinking Borders*/
-	for (y = 24; y >= 0; y--) {
-		x1 = MIN_X(y) - 1;
-		x2 = MAX_X(y) + 1;
-		change_value_by_pos(get_pos(x1, y) + 1, BORDER_COLOR);
-		change_value_by_pos(get_pos(x1, y), BORDER_VALUE);
-		change_value_by_pos(get_pos(x2, y) + 1, BORDER_COLOR);
-		change_value_by_pos(get_pos(x2, y), BORDER_VALUE);
-	}
-
+	init_board_content();
+	create_ball();
+	update_board();
 	display_board();
-	//create_cube();
 }
 
 void finish()
@@ -533,4 +551,81 @@ void finish()
 		INT  10h
 		INT	 27
 	}
+}
+
+void init_board_content()
+{
+	int x, x1, x2, y, pos_x1, pos_x2;
+	for (y = MIN_Y; y <= MAX_Y; y++)
+		for (x = MIN_X(MAX_Y); x <= MAX_X(MIN_Y); x++)
+			display_draft_attr[y][x] = display_attr[get_pos(x, y) / 2] = INIT_COLOR;  // blank
+
+	for (y = 0; y <= 24; y++)
+		for (x = MIN_X(y); x <= MAX_X(y); x++)
+			display_draft_attr[y][x] = display_attr[get_pos(x, y) / 2] = TRACK_COLOR;
+	
+	/*Blinking Borders*/
+	for (y = 24; y >= 0; y--) {
+		x1 = MIN_X(y) - 1;
+		x2 = MAX_X(y) + 1;
+		pos_x1 = get_pos(x1, y);
+		pos_x2 = get_pos(x2, y);
+		change_value_by_pos(pos_x1 + 1, BORDER_COLOR);
+		change_value_by_pos(pos_x1, BORDER_VALUE);
+		change_value_by_pos(pos_x2 + 1, BORDER_COLOR);
+		change_value_by_pos(pos_x2, BORDER_VALUE);
+		display_draft_attr[y][x1] = display_attr[pos_x1 / 2] = BORDER_COLOR;
+		display_draft_val[y][x1] = display_val[pos_x1 / 2] = BORDER_VALUE;
+		display_draft_attr[y][x2] = display_attr[pos_x2 / 2] = BORDER_COLOR;
+		display_draft_val[y][x2] = display_val[pos_x2 / 2] = BORDER_VALUE;
+	}
+	
+}
+
+void create_ball()
+{
+	int i, pos;
+	ball_pos = (POSITION*)(getmem(BALL_SIZE * sizeof(POSITION)));
+	if (ball_pos == NULL)
+		finish();
+	ball_pos[0].x = 38;	ball_pos[0].y = 23;
+	ball_pos[1].x = 38;	ball_pos[1].y = 24;
+	ball_pos[2].x = 39;	ball_pos[2].y = 23;
+	ball_pos[3].x = 39;	ball_pos[3].y = 24;
+
+	for (i = 0; i<BALL_SIZE; i++){
+		pos = get_pos(ball_pos[i].x, ball_pos[i].y);
+		display_draft_attr[ball_pos[i].y][ball_pos[i].x] = display_attr[pos / 2] = BALL_COLOR;
+		display_draft_val[ball_pos[i].y][ball_pos[i].x] = display_val[pos / 2] = BALL_VALUE;
+	}
+}
+
+void init_params()
+{
+	int i;
+	for(i=0;i<BALL_SIZE;i++)
+	{
+		jump_attr_save[i] = TRACK_COLOR;
+		jump_val_save[i] = TRACK_VALUE;
+	}
+}
+
+void game_over()
+{
+	int pos, y = 0, i=0,x;
+	char str[10] = "Game Over";
+	for(x=0;x<=9;x++)
+	{
+		pos = get_pos(x, y);
+		change_value_by_pos(pos + 1, WHITE_BLINKING_FOREGROUD);
+		change_value_by_pos(pos, str[i]);
+		i++;
+	}
+	/*for(i=0;i<BALL_COLOR;i++)
+	{
+		pos = get_pos(ball_pos[i].x, ball_pos[i].y);
+		change_value_by_pos(pos + 1, DEAD_BALL_COLOR);
+		change_value_by_pos(pos, DEAD_BALL_VALUE);
+	}*/
+	kill(dispid);
 }
